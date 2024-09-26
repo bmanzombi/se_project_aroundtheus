10793d3bacfb532d7e8a64d2721a887cd783438b
Blanchard Manzombi <bmanzombi@gmail.com>
2024-09-26 11:56:13 -0500

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialCarsds = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -30,4 +30,4 @@ const initialCarsds = [
   },
 ];
 
-console.log(initialCarsds);
+console.log(initialCards);
